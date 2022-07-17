8f8201e5b68f9f6e3e94a6da4dc1781d8b63c2ae
Ac Hsu <charliebookhsu@gmail.com>
2022-07-17 12:17:58 +0800

    add readme how to use the service ,and show current use http ip&port

--- SimpleHTTPServerWithUpload.sh
+++ SimpleHTTPServerWithUpload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 clear
-cd /mnt/shared_media
-python3 /bin/SimpleHTTPServerWithUpload.py 8080
+ip=$(eval ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+echo "http://$ip:8080 is listening..."
+python3 ./SimpleHTTPServerWithUpload.py 8080
